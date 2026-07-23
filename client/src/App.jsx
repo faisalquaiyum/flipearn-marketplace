e91527ac60cb170ac58c420fa0e46344dca8f82e
Faisal <mdfaisalquaiyum004@gmail.com>
2026-07-23 16:32:52 +0530

    admin pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,13 +10,20 @@ import Loading from "./pages/Loading";
 import Navbar from "./components/Navbar";
 import Chatbox from "./components/Chatbox";
 import { Toaster } from "react-hot-toast";
+import Layout from "./pages/admin/Layout";
+import Dashboard from "./pages/admin/Dashboard";
+import CredentialVerify from "./pages/admin/CredentialVerify";
+import CredentialChange from "./pages/admin/CredentialChange";
+import AllListings from "./pages/admin/AllListings";
+import Transactions from "./pages/admin/Transactions";
+import Withdrawal from "./pages/admin/Withdrawal";
 
 const App = () => {
-  const {pathname} = useLocation();
+  const { pathname } = useLocation();
   return (
     <>
       <Toaster />
-      {!pathname.includes('./admin') && <Navbar /> }
+      {!pathname.includes("./admin") && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/marketplace" element={<MarketPlace />} />
@@ -27,6 +34,14 @@ const App = () => {
         <Route path="/messages" element={<Messages />} />
         <Route path="/my-orders" element={<MyOrders />} />
         <Route path="/loading" element={<Loading />} />
+        <Route path="/admin" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="verify-credentials" element={<CredentialVerify />} />
+          <Route path="change-credentials" element={<CredentialChange />} />
+          <Route path="list-listings" element={<AllListings />} />
+          <Route path="transactions" element={<Transactions />} />
+          <Route path="withdrawal" element={<Withdrawal />} />
+        </Route>
       </Routes>
       <Chatbox />
     </>
